715eba6292bab65862cd58d858e4eed1d4f5f3ad
Dhairya Jain <jaindhairya2001@gmail.com>
2019-07-11 19:33:01 +0530

    Update models.py

--- First_Project/firstapp/models.py
+++ First_Project/firstapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length =264)
+    dob = models.DateField(default= date.today())
+    email = models.CharField(unique=True, max_length=100)
+
+class Company(models.Model):
+    com_id = models.ForeignKey(Person,'')
+    name = models.CharField(max_length=264)
