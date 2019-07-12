32e276244a0efd44ed5f9250eb1ab514ef8afb55
Dhairya Jain <jaindhairya2001@gmail.com>
2019-07-13 04:21:55 +0530

    Update urls.py

--- LevTwoProject/appTwo/urls.py
+++ LevTwoProject/appTwo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from appTwo import views
 
 urlpatterns = [
+	path('signup/',views.signup,name='signup'),
 	path('forms.html',views.form_view,name='form_view'),
 	path('',views.index,name='index'),
 	path('users/',views.users,name='users'),
